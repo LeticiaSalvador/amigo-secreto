1e284ad56dda669371a59b02e33072e1693801ca
LeticiaSalvador <leticiaalvessalvador@gmail.com>
2025-09-16 21:12:51 -0400

    terminando a criação

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+let amigos = [];
 
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+    if (nome.trim()) {
+        amigos.push(nome.trim());
+        document.querySelector('input').value = '';
+        atualizarLista();
+    }else{
+        alert('Por favor, insira um nome válido.');
+    }
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    amigos.forEach(function(amigo) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Adicione pelo menos um amigo para sortear.');
+        return;
+    }
+    const indice = Math.floor(Math.random() * amigos.length);
+    const sorteado = amigos[indice];
+    document.getElementById('resultado');//.textContent = `Amigo sorteado: ${sorteado}`;
+    resultado.innerHTML = `<li>Amigo sorteado: <strong>${sorteado}</strong></li>`;
+}
